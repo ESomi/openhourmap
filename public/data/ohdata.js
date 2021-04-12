c77cf1fc37aeb1a79cc201ad8a78ef4797d5db8c
ESomi <sweetsom27@gmail.com>
2021-04-13 04:41:55 +0900

    2020.12. openhour searchmap

---
+++ public/data/ohdata.js
@@ -0,0 +1,69 @@
+var data = [
+  { title: "어나더쏭",
+    url: "https://place.map.kakao.com/433187774",
+    openhour: "매일 10:00 - 22:00",
+    lat: 37.559270, 
+    lng: 127.086788,   
+  },{ title: "카페레디언트",
+  url: "https://place.map.kakao.com/757366145?openhour=1",
+  openhour: "월~금 08:00 ~ 22:00<br/>토,일 10:00 ~ 22:00",
+  lat: 37.557084838616014, 
+  lng: 127.08210906129733,   
+  },{ title: "커피홍당무",
+  url: "https://place.map.kakao.com/1050870377?openhour=1",
+  openhour: "휴무일 수요일<br/>월,화,목,금,토,일 12:00 ~ 21:00",
+  lat: 37.559921,
+  lng: 127.087040 
+  },{ title: "헤르츠커피",
+  url: "https://place.map.kakao.com/1508416506?openhour=1",
+  openhour: "휴무일 수요일<br/>화~일 09:00 ~ 22:00",
+  lat: 37.559290398597064,  
+  lng: 127.08092083693185 
+  },{ title: "하프커피",
+  url: "https://place.map.kakao.com/672096538", 
+  openhour: "매일 11:00 - 22:00",
+  lat: 37.5473912441155,
+  lng: 127.04231775319,   
+  },{ title: "오브코하우스",
+  url: "https://place.map.kakao.com/1364269549https://place.map.kakao.com/672096538?openhour=1",
+  openhour: "휴무일 화요일<br/>월,수,목,금,토,일 12:00 ~ 21:30",
+  lat: 37.54755477987921, //마커의 경위도는 127.04354451540102,37.54755477987921 입니다.
+  lng: 127.04354451540102,   
+  },{ title: "맛차차",
+  url: "https://place.map.kakao.com/505665583?openhour=1",
+  openhour: "휴무일 화요일<br/>수~일 11:00 ~ 19:00",
+  lat: 37.5468332303823, 
+  lng: 127.04059287436
+  },{ title: "카페성수",
+  url: "https://place.map.kakao.com/26643662?openhour=1",  
+  openhour: "매일 10:00 ~ 22:00",
+  lat: 37.5475425598847, 
+  lng: 127.042462685322 
+  },{ title: "트와블루",
+  url: "https://place.map.kakao.com/915374663?openhour=1",  
+  openhour: "토 11:00 ~ 23:00 </br>일 11:00 ~ 22:00 </br> 월~금 09:00 ~ 23:00",
+  lat: 37.5479063881466, 
+  lng: 127.042947224846 
+  },{ title: "빌로우",
+  url: "https://place.map.kakao.com/1505582117?openhour=1",  
+  openhour: "매일 12:00 ~ 22:00",
+  lat: 37.54723984630362, 
+  lng: 127.04240140670005 
+  },{ title: "모멘토브루어스",
+  url: "https://place.map.kakao.com/1435496905?openhour=1",  
+  openhour: "매일 11:00 ~ 19:00",
+  lat: 37.5474033576152,   
+  lng: 127.041185016539 
+  },{ title: "5TO7",
+  url: "https://place.map.kakao.com/328817208?openhour=1",  
+  openhour: "매일 11:30 ~ 21:00",
+  lat: 37.5460826295749,
+  lng: 127.043321867062 
+  },{ title: "앤아더",
+  url: "https://place.map.kakao.com/2022057939?openhour=1",  
+  openhour: "매일 10:00 ~ 23:00",
+  lat: 37.5463206978435, 
+  lng: 127.042760733457 
+  }
+];
+
